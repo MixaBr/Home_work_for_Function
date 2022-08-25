266d80fddb540bedb64ecf61d5109340c32de4f7
MixaBr <MixaBr@gmail.com>
2022-08-25 14:46:15 +0300

    blabla

--- HW_Recur.py
+++ HW_Recur.py
@@ -17,3 +17,14 @@ c = float(input("Введите сумму первоначального вкл
 y = int(input("На сколько лет размещаете депозит?:"))
 p = float(input("Под какой процент годовых размещается вклад?:"))
 print(depozit(c,y,p))
+
+#Hometask_13_4
+
+import random
+def rnd(am,mi,ma):
+    return [(int(random.randint(mi,ma))) for i in range(am)]
+
+a = int(input())
+i = int(input())
+m = int(input())
+print(rnd(a,i,m))
